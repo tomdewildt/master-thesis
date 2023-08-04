13e3acf4cea7de0c9d8e05e587e74d561ab3bdfe
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2023-08-04 23:33:11 +0200

    Add cloud-init config to terraform (#14)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 data "template_cloudinit_config" "cic" {
-  gzip          = true
+  gzip          = false
   base64_encode = true
 
   part {
@@ -7,26 +7,135 @@ data "template_cloudinit_config" "cic" {
     content_type = "text/cloud-config"
     content      = <<EOF
     #cloud-config
+    locale: en_US.UTF-8
+    timezone: Europe/Amsterdam
     package_update: true
     package_upgrade: true
-    packages: ['python3-pip']
+    packages:
+      - python3
+      - python3-pip
+      - ufw
+      - fail2ban
+    write_files:
+      - path: /etc/sysctl.d/10-configure-ipv6.conf
+        permissions: 0644
+        content: |
+          net.ipv6.conf.all.disable_ipv6=1
+          net.ipv6.conf.default.disable_ipv6=1
+          net.ipv6.conf.lo.disable_ipv6=1
+          net.ipv6.conf.eth0.disable_ipv6=1
+      - path: /etc/sysctl.d/10-configure-logging.conf
+        permissions: 0644
+        content: |
+          kernel.printk = 3 4 1 3
+      - path: /etc/ssh/sshd_config
+        permissions: 0644
+        content: |
+          Port 22
+          SyslogFacility AUTH
+          LogLevel INFO
+          PermitRootLogin no
+          PermitEmptyPasswords no
+          ChallengeResponseAuthentication no
+          HostbasedAuthentication no
+          PasswordAuthentication no
+          PubkeyAuthentication yes
+          LoginGraceTime 2m
+          MaxAuthTries 6
+          MaxSessions 10
+          StrictModes yes
+          UsePAM yes
+          UseDNS no
+          HostKey /etc/ssh/ssh_host_rsa_key
+          HostKey /etc/ssh/ssh_host_ecdsa_key
+          HostKey /etc/ssh/ssh_host_ed25519_key
+          IgnoreRhosts yes
+          AcceptEnv LANG LC_*
+          X11Forwarding no
+          PrintMotd no
+          Subsystem sftp /usr/lib/openssh/sftp-server
+      - path: /etc/fail2ban/jail.d/defaults-debian.conf
+        permissions: 0644
+        content: |
+          [DEFAULT]
+          ignoreself = true
+          bantime  = 1h
+          findtime = 30m
+          maxretry = 6
+      - path: /etc/fail2ban/jail.d/sshd.conf
+        permissions: 0644
+        content: |
+          [sshd]
+          enabled  = true
+          mode     = aggressive
+          port     = ssh
+          logpath  = %(sshd_log)s
+          backend  = %(sshd_backend)s
+          bantime  = 24h
+          findtime = 12h
+          maxretry = 3
+      - path: /usr/lib/systemd/system/jupyter.service
+        permissions: 0644
+        content: |
+          [Unit]
+          Description=Jupyter Notebook
+          [Service]
+          Type=simple
+          PIDFile=/run/jupyter.pid
+          ExecStart=/home/${var.username}/.local/bin/jupyter-notebook --config=/home/${var.username}/.jupyter/jupyter_notebook_config.py
+          User=${var.username}
+          Group=${var.username}
+          WorkingDirectory=/home/${var.username}
+          Restart=always
+          RestartSec=10
+          [Install]
+          WantedBy=multi-user.target
+    runcmd:
+      - sed -i "/IPV6=/c\IPV6=no" /etc/default/ufw
+      - ufw default deny incoming
+      - ufw default allow outgoing
+      - ufw allow 22 comment "Allow SSH"
+      - ufw allow 8888 comment "Allow Jupyter"
+      - ufw --force enable
+      - sudo -H -i -u ${var.username} bash -c "pip3 install -U jsonschema jupyter zipp"
+      - sudo -H -i -u ${var.username} bash -c "jupyter notebook --generate-config"
+      - echo "" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - echo "## Custom config" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - echo "c.ServerApp.ip = '0.0.0.0'" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - echo "c.ServerApp.port = 8888" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - echo "c.ServerApp.port_retries = 0" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - echo "c.ServerApp.open_browser = False" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - echo "c.ServerApp.quit_button = False" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - echo "c.PasswordIdentityProvider.password_required = True" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - echo "c.PasswordIdentityProvider.allow_password_change = True" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - echo "c.PasswordIdentityProvider.hashed_password = 'sha512::${sha512(var.password != null ? var.password : random_password.pw.result)}'" >> /home/${var.username}/.jupyter/jupyter_notebook_config.py
+      - systemctl enable jupyter.service
+      - systemctl restart jupyter.service
     EOF
   }
 }
 
+resource "random_password" "pw" {
+  length  = 16
+  lower   = true
+  upper   = true
+  numeric = true
+  special = false
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.namespace}-vm-${var.environment}-${var.location}-01"
-  location = "westeurope"
+  name     = "${var.namespace}-${var.environment}-${var.location}"
+  location = var.location
 }
 
 resource "azurerm_network_watcher" "nw" {
-  name                = "nw-${var.namespace}-vm-${var.environment}-${var.location}-01"
+  name                = "${var.namespace}-${var.name}-nw-${var.environment}-${var.location}-01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${var.namespace}-vm-${var.environment}-${var.location}-01"
+  name                = "${var.namespace}-${var.name}-vnet-${var.environment}-${var.location}-01"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -34,21 +143,21 @@ resource "azurerm_virtual_network" "vnet" {
 
 
 resource "azurerm_subnet" "snet" {
-  name                 = "snet-${var.namespace}-vm-${var.environment}-${var.location}-01"
+  name                 = "${var.namespace}-${var.name}-snet-${var.environment}-${var.location}-01"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
-  name                = "pip-${var.namespace}-vm-${var.environment}-${var.location}-01"
+  name                = "${var.namespace}-${var.name}-pip-${var.environment}-${var.location}-01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg-${var.namespace}-vm-${var.environment}-${var.location}-01"
+  name                = "${var.namespace}-${var.name}-nsg-${var.environment}-${var.location}-01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -63,15 +172,27 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "AllowJupyterInBound"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8888"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "nic-${var.namespace}-vm-${var.environment}-${var.location}-01"
+  name                = "${var.namespace}-${var.name}-nic-${var.environment}-${var.location}-01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "nc-${var.namespace}-vm-${var.environment}-${var.location}-01"
+    name                          = "${var.namespace}-${var.name}-nc-${var.environment}-${var.location}-01"
     subnet_id                     = azurerm_subnet.snet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
@@ -89,14 +210,14 @@ resource "tls_private_key" "ssh" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = "vm-${var.namespace}-vm-${var.environment}-${var.location}-01"
+  name                  = "${var.namespace}-${var.name}-vm-${var.environment}-${var.location}-01"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   size                  = var.type == "gpu" ? "Standard_NC6s_v3" : "Standard_B1s"
 
   os_disk {
-    name                 = "osdisk-${var.namespace}-vm-${var.environment}-${var.location}-01"
+    name                 = "${var.namespace}-${var.name}-osdisk-${var.environment}-${var.location}-01"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -108,9 +229,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
-  computer_name                   = "vm"
+  computer_name                   = "${var.namespace}-${var.name}-${var.environment}-${var.location}-01"
   user_data                       = data.template_cloudinit_config.cic.rendered
   admin_username                  = var.username
+  admin_password                  = var.password != null ? var.password : random_password.pw.result
   disable_password_authentication = true
 
   admin_ssh_key {
@@ -120,7 +242,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 }
 
 resource "azurerm_virtual_machine_extension" "vme" {
-  name                 = "vm"
+  name                 = "${var.namespace}-${var.name}-${var.environment}-${var.location}-01"
   virtual_machine_id   = azurerm_linux_virtual_machine.vm.id
   publisher            = "Microsoft.HpcCompute"
   type                 = "NvidiaGpuDriverLinux"
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -6,6 +6,16 @@ output "address" {
   value = azurerm_linux_virtual_machine.vm.public_ip_address
 }
 
+output "username" {
+  value     = var.username
+  sensitive = true
+}
+
+output "password" {
+  value     = var.password != null ? var.password : random_password.pw.result
+  sensitive = true
+}
+
 output "private_key" {
   value     = tls_private_key.ssh.private_key_pem
   sensitive = true
