8e21cd89cff0e42020d939f9b1c00878d52480c3
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2023-08-04 23:32:10 +0200

    Add cloud-init config to terraform

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,21 +4,33 @@ variable "namespace" {
   default     = "thesis"
 }
 
+variable "name" {
+  type        = string
+  description = "The name used for all resources."
+  default     = "jupyter"
+}
+
 variable "location" {
   type        = string
   description = "The location used for all resources."
-  default     = "west-eu"
+  default     = "westeurope"
 }
 
 variable "environment" {
   type        = string
   description = "The environment used for all resources."
   default     = "dev"
+
+  validation {
+    condition     = var.environment == "dev" || var.environment == "test" || var.environment == "prod"
+    error_message = "The environment must be dev, test, or prod."
+  }
 }
 
 variable "type" {
   type        = string
   description = "The type of virtual machine to deploy (cpu or gpu)."
+  default     = "cpu"
 
   validation {
     condition     = var.type == "cpu" || var.type == "gpu"
@@ -28,7 +40,14 @@ variable "type" {
 
 variable "username" {
   type        = string
-  description = "The username used for the virutal machine."
-  default     = "tomdewildt"
+  description = "The username used for the virtual machine."
+  default     = "jupyter"
+  sensitive   = true
 }
 
+variable "password" {
+  type        = string
+  description = "The password used for the virtual machine."
+  default     = null
+  sensitive   = true
+}
