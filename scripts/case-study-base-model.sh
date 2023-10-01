5eb20ae8a18b553245451fa54dda017a111ee6a5
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2023-10-01 23:26:37 +0200

    Remove logs folder from base model job

--- scripts/case-study-base-model.sh
+++ scripts/case-study-base-model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=case-study-base-model
-#SBATCH --output=./logs/case-study-base-model-%j.out
-#SBATCH --error=./logs/case-study-base-model-%j.error
+#SBATCH --output=case-study-base-model-%j.out
+#SBATCH --error=case-study-base-model-%j.error
 #SBATCH --chdir /home/tdewildt/master-thesis
 #SBATCH --export=ALL
 #SBATCH --get-user-env=L
