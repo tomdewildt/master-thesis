0f147339a7af21557bccac8da3840ae086b569ec
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2023-10-01 21:59:45 +0200

    Fix variable subtitution in base model job

--- scripts/case-study-base-model.sh
+++ scripts/case-study-base-model.sh
@@ -23,8 +23,8 @@ module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
 module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0
 
 # Logging
-echo "PYTHON_VERSION = ${python --version}"
-echo "PIP_VERSION = ${pip --version}"
+echo "PYTHON_VERSION = $(python --version)"
+echo "PIP_VERSION = $(pip --version)"
 
 # Run experiment
 python -m experiments.case_study_base_model
