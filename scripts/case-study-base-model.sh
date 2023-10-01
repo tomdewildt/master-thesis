da4212572c38a09a98df7a5bbbcd882ffe32a546
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2023-10-01 21:42:47 +0200

    Add aiohttp module to base model job script

--- scripts/case-study-base-model.sh
+++ scripts/case-study-base-model.sh
@@ -15,11 +15,12 @@ export PYTHONPATH=${HOME}/master-thesis/src
 # Setup modules
 module purge
 module load 2022
-module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
-module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
-module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0
-module load SciPy-bundle/2022.05-foss-2022a
+module load aiohttp/3.8.3-GCCcore-11.3.0
 module load matplotlib/3.5.2-foss-2022a
+module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
+module load SciPy-bundle/2022.05-foss-2022a
+module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
+module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0
 
 # Run experiment
 python -m experiments.case_study_base_model
