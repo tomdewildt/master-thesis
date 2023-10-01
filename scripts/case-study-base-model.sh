b575e8f7dea9aae91731a3473411b0a7875738e6
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2023-10-01 22:07:12 +0200

    Add dependencies to base model job

--- scripts/case-study-base-model.sh
+++ scripts/case-study-base-model.sh
@@ -9,9 +9,6 @@
 #SBATCH --gpus=1
 #SBATCH --time=00:01:00
 
-# Include package
-export PYTHONPATH=${HOME}/master-thesis/src
-
 # Setup modules
 module purge
 module load 2022
@@ -22,9 +19,36 @@ module load SciPy-bundle/2022.05-foss-2022a
 module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
 module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0
 
-# Logging
+# Log versions
 echo "PYTHON_VERSION = $(python --version)"
 echo "PIP_VERSION = $(pip --version)"
+
+# Setup environment
+export PYTHONPATH=${HOME}/master-thesis/src
+
+# Setup dependencies
+pip install \
+    accelerate \
+    anthropic \
+    bert-score \
+    bitsandbytes \
+    git+https://github.com/google-research/bleurt.git \
+    datasets \
+    google-cloud-aiplatform \
+    langchain \
+    nltk \
+    openai \
+    peft \
+    protobuf \
+    rapidfuzz \
+    rouge-score \
+    scipy \
+    sentencepiece \
+    tiktoken \
+    torch \
+    transformers \
+    trl \
+    xformers
 
 # Run experiment
 python -m experiments.case_study_base_model
