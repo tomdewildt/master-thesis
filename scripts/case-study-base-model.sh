db649855e57c02e4c139da487d87cd1b468452e0
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2023-10-01 22:17:51 +0200

    Add logging to base model job

--- scripts/case-study-base-model.sh
+++ scripts/case-study-base-model.sh
@@ -10,6 +10,7 @@
 #SBATCH --time=00:30:00
 
 # Setup modules
+echo "[JOB] Loading modules"
 module purge
 module load 2022
 module load aiohttp/3.8.3-GCCcore-11.3.0
@@ -18,16 +19,22 @@ module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
 module load SciPy-bundle/2022.05-foss-2022a
 module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
 module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0
+echo
 
 # Log versions
+echo "[JOB] Logging versions"
 echo "PYTHON_VERSION = $(python --version)"
 echo "PIP_VERSION = $(pip --version)"
+echo
 
 # Setup environment
+echo "[JOB] Setting up environment"
 export PYTHONPATH=${HOME}/master-thesis/src
+echo
 
 # Setup dependencies
-pip install \
+echo "[JOB] Setting up dependencies"
+pip install -q \
     accelerate \
     anthropic \
     bert-score \
@@ -49,6 +56,9 @@ pip install \
     transformers \
     trl \
     xformers
+echo
 
 # Run experiment
+echo "[JOB] Running experiment"
 python -m experiments.case_study_base_model
+echo
