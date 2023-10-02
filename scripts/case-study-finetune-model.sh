55d7d4ef270e636e14ae467ed440204e4acdaf62
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2023-10-02 15:44:36 +0200

    Only load modules when running in SLURM environment

--- scripts/case-study-finetune-model.sh
+++ scripts/case-study-finetune-model.sh
@@ -12,18 +12,20 @@
 #SBATCH --mail-user=t.l.dewildt@tilburguniversity.edu
 
 # Setup modules
-module purge
-module load 2022
-module load aiohttp/3.8.3-GCCcore-11.3.0
-module load matplotlib/3.5.2-foss-2022a
-module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
-module load SciPy-bundle/2022.05-foss-2022a
-module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
-module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0
+if [ -z "${SLURM_JOB_ID}" ]; then
+    module purge
+    module load 2022
+    module load aiohttp/3.8.3-GCCcore-11.3.0
+    module load matplotlib/3.5.2-foss-2022a
+    module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
+    module load SciPy-bundle/2022.05-foss-2022a
+    module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
+    module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0
+fi
 
 # Log versions
-echo "PYTHON_VERSION = $(python --version)"
-echo "PIP_VERSION = $(pip --version)"
+echo "PYTHON_VERSION = $(python3.10 --version)"
+echo "PIP_VERSION = $(pip3.10 --version)"
 
 # Setup environment
 if [ -f .env ]; then
@@ -32,7 +34,7 @@ fi
 export PYTHONPATH=src
 
 # Setup dependencies
-pip install -q \
+pip3.10 install -q \
     accelerate \
     anthropic \
     bert-score \
@@ -56,4 +58,4 @@ pip install -q \
     xformers
 
 # Run experiment
-python -m experiments.case_study_finetune_model
+python3.10 -m experiments.case_study_finetune_model
